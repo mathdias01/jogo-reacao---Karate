a5920fa1e3836f323f82c6d14d5075664708c812
mathdias01 <dias.moutinho34@gmail.com>
2026-09-09 14:57:19 +0000

    implementa selecao de dificuldade

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
+const telaMenu = document.getElementById("tela-menu");
+const telaJogo = document.getElementById("tela-jogo");
+const telaResultado = document.getElementById("tela-resultado");
+
+const botoesDificuldade = document.querySelectorAll(".botao-dificuldade")
+
+
+
+botoesDificuldade.forEach(function(botao) {
+
+    console.log("Encontrei um botão!");
+
+    botao.addEventListener("click", function(){
+        console.log("CLIQUEI!");
+        console.log(botao.dataset);
+    });
+
+});
+
+
+console.log("Teste")
+
+
+
+
 
